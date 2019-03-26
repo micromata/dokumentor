c9d8ca6d435d865de0ea89f7432120be09efc90b
Christian Stein <sormuras@gmail.com>
2019-03-26 12:00:44 +0100

    Implement creator delegation

--- src/main/java/de/micromata/dokumentor/Dokumentor.java
+++ src/main/java/de/micromata/dokumentor/Dokumentor.java
@@ -1,3 +1,27 @@
 package de.micromata.dokumentor;
 
-public class Dokumentor {}
+import com.fnproject.fn.api.httpgateway.HTTPGatewayContext;
+
+public class Dokumentor implements Creator {
+
+  @Override
+  public byte[] create(HTTPGatewayContext context) {
+    String key = context.getQueryParameters().get("creator").orElse("qr");
+    Creator creator = createCreator(context, key);
+    return creator.create(context);
+  }
+
+  private Creator createCreator(HTTPGatewayContext context, String key) {
+    switch (key.toLowerCase()) {
+      case "qr":
+        return new QRGen(context.getInvocationContext().getRuntimeContext());
+      case "txt":
+      case "xml":
+        // return new TextTransformer();
+      case "doc":
+        throw new UnsupportedOperationException("Doc files aren't supported, yet");
+      default:
+        throw new RuntimeException("Unknown transformer key: " + key);
+    }
+  }
+}
